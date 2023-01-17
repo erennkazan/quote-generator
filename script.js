f4d35690121892e970713f442ed2141b839a7eca
erennkazan <erennkazan@gmail.com>
2023-01-18 00:44:24 +0300

    loader added

--- script.js
+++ script.js
@@ -3,10 +3,26 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
 let apiQuotes = [];
+
+//show loading
+function loading(){
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+//hide loading
+function complete(){
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 
 //show new quote
 function newQuote(){
+    loading();
     //pick a random quote from apiquotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     //chech if author field is blank and replace it witch 'unknown'
@@ -22,13 +38,15 @@ function newQuote(){
     }else{
         quoteText.classList.remove('long-quote');
     }
+    //set quote,hide loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
 
 //get quotes from api
 async function getQuotes() {
-
+    loading();
     const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
     try {
         const response = await fetch(apiUrl);
